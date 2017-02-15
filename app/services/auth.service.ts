b5046b83814edbb165e65ea86065fe12bb4f22e5
Antharam Shiva Kumar <antharamshivakumar@gmail.com>
2017-02-15 09:06:05 +0000

    Add files via upload

---
+++ app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {tokenNotExpired} from 'angular2-jwt';
+
+declare var Auth0Lock: any;
+
+@Injectable()
+export class Auth {
+    // Configure Auth0
+    lock = new Auth0Lock('ePf5C6vplEwUusr1wmth3LkHwqtmxzUF', 'techguyweb.auth0.com', {});
+    
+    constructor(){
+         // Add callback for lock `authenticated` event
+        this.lock.on("authenticated", (authResult:any) => {
+            this.lock.getProfile(authResult.idToken, function(error:any, profile:any){
+                if(error){
+                    throw new Error(error);
+                }
+                // Set Profile
+                localStorage.setItem('profile', JSON.stringify(profile));
+                // Set Token
+                localStorage.setItem('id_token', authResult.idToken);
+            })
+        });
+    }
+    
+    public login() {
+        // Call the show method to display the widget.
+        this.lock.show();
+    };
+    
+    public authenticated(){
+        return tokenNotExpired();
+    }
+    
+    public logout() {
+        // Remove info from localStorage
+        localStorage.removeItem('id_token');
+        localStorage.removeItem('profile');
+    };
+}
